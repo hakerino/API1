e23beeef30ed3d31fe7fb8b83b1805996c043bfd
Elien <Elien@LAPTOP-H26IA7OF>
2025-05-15 00:28:29 +0200

    Aggiungere i file di progetto.

---
+++ API1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace API1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCerca = new System.Windows.Forms.Button();
+            this.textBoxParola = new System.Windows.Forms.TextBox();
+            this.listBoxDefinizioni = new System.Windows.Forms.ListBox();
+            this.buttonCaricaLibro = new System.Windows.Forms.Button();
+            this.listBoxContrari = new System.Windows.Forms.ListBox();
+            this.listBoxSinonimi = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCerca
+            // 
+            this.buttonCerca.BackColor = System.Drawing.Color.Coral;
+            this.buttonCerca.Location = new System.Drawing.Point(360, 167);
+            this.buttonCerca.Name = "buttonCerca";
+            this.buttonCerca.Size = new System.Drawing.Size(97, 46);
+            this.buttonCerca.TabIndex = 0;
+            this.buttonCerca.Text = "Definizione";
+            this.buttonCerca.UseVisualStyleBackColor = false;
+            // 
+            // textBoxParola
+            // 
+            this.textBoxParola.Location = new System.Drawing.Point(259, 219);
+            this.textBoxParola.Name = "textBoxParola";
+            this.textBoxParola.Size = new System.Drawing.Size(198, 20);
+            this.textBoxParola.TabIndex = 1;
+            // 
+            // listBoxDefinizioni
+            // 
+            this.listBoxDefinizioni.BackColor = System.Drawing.Color.Moccasin;
+            this.listBoxDefinizioni.FormattingEnabled = true;
+            this.listBoxDefinizioni.Location = new System.Drawing.Point(463, 92);
+            this.listBoxDefinizioni.Name = "listBoxDefinizioni";
+            this.listBoxDefinizioni.Size = new System.Drawing.Size(249, 264);
+            this.listBoxDefinizioni.TabIndex = 2;
+            // 
+            // buttonCaricaLibro
+            // 
+            this.buttonCaricaLibro.BackColor = System.Drawing.Color.Coral;
+            this.buttonCaricaLibro.Location = new System.Drawing.Point(259, 167);
+            this.buttonCaricaLibro.Name = "buttonCaricaLibro";
+            this.buttonCaricaLibro.Size = new System.Drawing.Size(95, 46);
+            this.buttonCaricaLibro.TabIndex = 5;
+            this.buttonCaricaLibro.Text = "Sinonimi e contrari";
+            this.buttonCaricaLibro.UseVisualStyleBackColor = false;
+            this.buttonCaricaLibro.Click += new System.EventHandler(this.buttonCaricaLibro_Click_1);
+            // 
+            // listBoxContrari
+            // 
+            this.listBoxContrari.BackColor = System.Drawing.Color.Moccasin;
+            this.listBoxContrari.FormattingEnabled = true;
+            this.listBoxContrari.Location = new System.Drawing.Point(15, 222);
+            this.listBoxContrari.Name = "listBoxContrari";
+            this.listBoxContrari.Size = new System.Drawing.Size(238, 134);
+            this.listBoxContrari.TabIndex = 6;
+            // 
+            // listBoxSinonimi
+            // 
+            this.listBoxSinonimi.BackColor = System.Drawing.Color.Moccasin;
+            this.listBoxSinonimi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxSinonimi.FormattingEnabled = true;
+            this.listBoxSinonimi.Location = new System.Drawing.Point(15, 92);
+            this.listBoxSinonimi.Name = "listBoxSinonimi";
+            this.listBoxSinonimi.Size = new System.Drawing.Size(238, 119);
+            this.listBoxSinonimi.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(106, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(553, 73);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "English dictionary";
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::API1.Properties.Resources.libreria;
+            this.ClientSize = new System.Drawing.Size(724, 381);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxSinonimi);
+            this.Controls.Add(this.listBoxContrari);
+            this.Controls.Add(this.buttonCaricaLibro);
+            this.Controls.Add(this.listBoxDefinizioni);
+            this.Controls.Add(this.textBoxParola);
+            this.Controls.Add(this.buttonCerca);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCerca;
+        private System.Windows.Forms.TextBox textBoxParola;
+        private System.Windows.Forms.ListBox listBoxDefinizioni;
+        private System.Windows.Forms.Button buttonCaricaLibro;
+        private System.Windows.Forms.ListBox listBoxContrari;
+        private System.Windows.Forms.ListBox listBoxSinonimi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ API1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
+namespace API1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonCerca.Click += ButtonCerca_Click;
+        }
+
+        //private void buttonCaricaLibro_Click(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog openFileDialog = new OpenFileDialog();
+        //    openFileDialog.Filter = "Text Files (*.txt)|*.txt";
+
+        //    if (openFileDialog.ShowDialog() == DialogResult.OK)
+        //    {
+        //        string[] righe = File.ReadAllLines(openFileDialog.FileName);
+
+        //        listBoxCapitoli.Items.Clear();
+
+        //        string capitoloCorrente = "";
+        //        List<string> testoCapitolo = new List<string>();
+
+        //        foreach (string riga in righe)
+        //        {
+        //            if (riga.StartsWith("Chapter", StringComparison.OrdinalIgnoreCase) || riga.StartsWith("CHAPTER"))
+        //            {
+        //                if (!string.IsNullOrEmpty(capitoloCorrente))
+        //                {
+        //                    listBoxCapitoli.Items.Add(new
+        //                    {
+        //                        Titolo = capitoloCorrente,
+        //                        Contenuto = string.Join(Environment.NewLine, testoCapitolo)
+        //                    });
+        //                }
+
+        //                capitoloCorrente = riga.Trim();
+        //                testoCapitolo.Clear();
+        //            }
+        //            else
+        //            {
+        //                testoCapitolo.Add(riga);
+        //            }
+        //        }
+
+        //        if (!string.IsNullOrEmpty(capitoloCorrente))
+        //        {
+        //            listBoxCapitoli.Items.Add(new
+        //            {
+        //                Titolo = capitoloCorrente,
+        //                Contenuto = string.Join(Environment.NewLine, testoCapitolo)
+        //            });
+        //        }
+
+        //        listBoxCapitoli.DisplayMember = "Titolo";
+        //    }
+        //}
+
+        //private void listBoxCapitoli_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    var item = listBoxCapitoli.SelectedItem;
+        //    if (item != null)
+        //    {
+        //        var prop = item.GetType().GetProperty("Contenuto");
+        //        string contenuto = prop.GetValue(item)?.ToString();
+        //        listBoxCapitoli.Text = contenuto;
+        //    }
+        //}
+
+        private async void ButtonCerca_Click(object sender, EventArgs e)
+        {
+            string parola = textBoxParola.Text.Trim().ToLower();
+            if (string.IsNullOrWhiteSpace(parola))
+            {
+                MessageBox.Show("Inserisci una parola da cercare.");
+                return;
+            }
+
+            await CercaDefinizione(parola);
+        }
+        private async Task CercaSinonimiEContrari(string parola)
+        {
+            listBoxSinonimi.Items.Clear();
+            listBoxContrari.Items.Clear();
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string urlSinonimi = $"https://api.datamuse.com/words?rel_syn={parola}";
+                    string jsonSinonimi = await client.GetStringAsync(urlSinonimi);
+                    JArray sinonimi = JArray.Parse(jsonSinonimi);
+
+                    listBoxSinonimi.Items.Add("Sinonimi:");
+                    foreach (var item in sinonimi)
+                    {
+                        listBoxSinonimi.Items.Add(item["word"].ToString());
+                    }
+
+                    string urlContrari = $"https://api.datamuse.com/words?rel_ant={parola}";
+                    string jsonContrari = await client.GetStringAsync(urlContrari);
+                    JArray contrari = JArray.Parse(jsonContrari);
+
+                    listBoxContrari.Items.Add("Contrari:");
+                    foreach (var item in contrari)
+                    {
+                        listBoxContrari.Items.Add(item["word"].ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Errore nel recupero di sinonimi e contrari.");
+                }
+            }
+        }
+
+        private async Task CercaDefinizione(string parola)
+        {
+            listBoxDefinizioni.Items.Clear();
+
+            string url = $"https://api.dictionaryapi.dev/api/v2/entries/en/{parola}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string json = await client.GetStringAsync(url);
+                    JArray array = JArray.Parse(json);
+
+                    foreach (var meaning in array[0]["meanings"])
+                    {
+                        string parteDelDiscorso = meaning["partOfSpeech"]?.ToString();
+                        listBoxDefinizioni.Items.Add($"--- {parteDelDiscorso} ---");
+
+                        foreach (var def in meaning["definitions"])
+                        {
+                            string definizione = def["definition"]?.ToString();
+                            string esempio = def["example"]?.ToString();
+
+                            listBoxDefinizioni.Items.Add($"Definizione: {definizione}");
+                            if (!string.IsNullOrEmpty(esempio))
+                            {
+                                listBoxDefinizioni.Items.Add($"Esempio: {esempio}");
+                            }
+                            listBoxDefinizioni.Items.Add(""); 
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    listBoxDefinizioni.Items.Add("Errore di connessione all'API.");
+                }
+                catch (Exception ex)
+                {
+                    listBoxDefinizioni.Items.Add("Parola non trovata o errore nella risposta.");
+                }
+            }
+        }
+
+        private async void buttonCaricaLibro_Click_1(object sender, EventArgs e)
+        {
+            
+            string parola = textBoxParola.Text.Trim().ToLower();
+            if (string.IsNullOrWhiteSpace(parola))
+            {
+                MessageBox.Show("Inserisci una parola da cercare.");
+                return;
+            }
+
+            await CercaDefinizione(parola);
+            await CercaSinonimiEContrari(parola);
+        }
+    }
+}
+
